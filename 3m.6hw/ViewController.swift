b4a010b8a5feb98bd51357fa96c0ce0144f821a9
mmeerimn <mamatkadyrovameerim05@gmail.com>
2023-06-05 20:03:03 +0600

    Initial Commit

---
+++ 3m.6hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3m.6hw
+//
+//  Created by Meerim Mamatkadyrova on 5/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
